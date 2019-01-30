0a66f440953a8f5a4a22bde649513ae557beba4b
Moses Olawoyin <m.bastford@gmail.com>
2019-01-30 17:36:37 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
 def my_each(array)
   counter = 0 
   while counter < array.length
-  
-  yield(array[counter])
+    yield(array[counter])
     counter += 1
   end
-   my_each(array) do |i|
-     puts i
-   end
+   
 end
